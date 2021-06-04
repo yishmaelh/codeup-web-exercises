2b88dd6623ad671dded7c87811949445310c7d9e
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-06-04 16:35:49 -0500

    added prompt loop

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,10 +1,17 @@
 "use strict"
 
-var random = prompt("Enter an odd number between 1 and 50 ")
-
-
+while(true){
+    var random = parseFloat(prompt("Enter an odd number between 1 and 50 "));
+    if(random % 2 != 0){
+        break
+    }
+}
 
 console.log("The random odd number skipped is: " + random);
+
+
+
+
 
 
 for ( let i = 1; i <= 50; i++) {
@@ -19,4 +26,8 @@ for ( let i = 1; i <= 50; i++) {
         console.log("This is an odd number: " + i);
     }
 }
+
+
+
+
 
