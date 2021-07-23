3209ad623b3668dfd757e73787368390eb0d5fec
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-07-23 13:38:36 -0500

    remove key

--- js/keys.js
+++
@@ -1,3 +0,0 @@
-const MAPBOX_KEY = "pk.eyJ1IjoieWlzaGgiLCJhIjoiY2tyNmpldnlrMHNxaDJwbnZiYzgzZGZmNSJ9.ftc2b8HOJdd_n-CIb67xZg";
-const OPEN_WEATHER_API_KEY = "4edf41fef9f2ffbed922f6b6c95b4493";
-const accessToken =
