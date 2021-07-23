4fe69d4475b2671fb58acf178a51a3ac507b8116
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-07-23 14:24:24 -0500

    can retrieve last commit

--- js/promises.js
+++ js/promises.js
@@ -3,6 +3,11 @@
 
 
 function lastCommit(userName) {
- return fetch('https://api.github.com/users/userName/events/public', {headers: {'Authorization': accessToken}});
-
+ return fetch(`https://api.github.com/users/${userName}/events/public`, {
+  headers: {'Authorization': accessToken}}).then(response => {
+   return response.json();
+ }).then(data => console.log('Last commit was :', data[0].created_at))
+     .catch(error => console.error('An error has occured', error));
 }
+
+console.log(lastCommit('yishmaelh'));
